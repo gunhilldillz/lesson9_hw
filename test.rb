50384a368f2d51c532cc553b83921d0a3e5b1a40
Joseph Rivera <josephrivera@jmr1.com>
2014-02-13 22:56:13 -0500

    removed test.rb

--- test.rb
+++
@@ -1,8 +0,0 @@
-# Joseph Rivera
-# February 11 2014
-# do not use a for loop to increment i -- instead use while loops
-# each loops use these most - learn it! can interate by more than one in each loops
-# a function or METHOD is very much like a math function
-# object is like blueprint of a house - description of real thing -- every instance of the blueprint is an instance in and of itself but it is based on the blueprint
-#advanced rest client -- download
-
